7f0439f906ab0db9ab48a763d6e785a956042f06
Thomas Legris <thomas.legris@idnow.io>
2026-03-31 15:17:59 +0200

    Update DocIDVAI to version 1.6.0

---
+++ CHANGELOG.md
@@ -0,0 +1,59 @@
+# Changelog
+
+## [1.6.0] - 2025-03-31
+### Added
+- eID Governikus feature support
+
+## [1.4.0] - 2025-03-02
+### Added
+- Automatic recovery when connection is lost and restored during identification.
+
+### Fixed
+- Several bugfixes and technical improvements. 
+
+## [1.3.0] - 2025-02-12
+### Added
+- Full country names translation in screens
+
+### Fixed
+- Stability enhancements about
+
+## [1.2.0] - 2025-01-22
+### Added
+- Further progress toward full WCAG 2.1 compliance.
+- More components updated for improved accessibility.
+- Liveness now accessible across multiple backend vendor options.
+- Configurable design themes tailored to customer requirements.
+- Custom Lottie animations now supported.
+
+### Changed
+- Overall UX refinements
+
+### Fixed
+- Functional and accessibility bug fixes
+- Stability enhancements
+
+## [1.1.0] - 2025-12-22
+### Fixed
+- Fixed a liveness video issue when a NFC flow completed before
+
+## [1.1.0] - 2025-11-19
+### Added
+- Screen redesign to comply with iOS accessibility guidelines.
+- Support for light and dark mode.
+- Adjustable text size and spacing (honors system preferences).
+- Improved compatibility with Voice Over (labels, focus order, actions).
+
+### Changed
+- Enhanced color contrast for better readability.
+- Visible and consistent focus indicators.
+- Proper content descriptions for images and icons.
+- Clear announcements for component states (error, selected, disabled, progress).
+
+### Fixed
+- Optimized memory usage and app responsiveness.
+- Several bugfixes to improve user experience.
+
+## [1.0.0] - 2025-06-24
+### Added
+- Initial version of the DocIDV SDK with the AutoIdent flow support
---
+++ Package.swift
@@ -0,0 +1,78 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DocIDVAI",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "DocIDVAI",
+            targets: ["DocIDVAI-Target-Wrapper"]
+        ),
+        .library(
+            name: "DocIDVAI-with-XS2A",
+            targets: ["DocIDVAI-with-XS2A-Target-Wrapper"]
+        )
+    ],
+    // Define external dependencies (via SPM).
+    dependencies: [
+        .package(url: "https://github.com/idnow/sunflower-sdk-ios.git", exact: "2.1.4"),
+        .package(url: "https://github.com/unissey/sdk-ios.git", exact: "4.0.0"),
+    ],
+    targets: [
+        // Define our 2 SDK internal binaries and their location.
+        .binaryTarget(
+            name: "DocIDVAI",
+            url: "https://github.com/idnow/docidv-ai-ios/releases/download/1.6.0/DocIDVAI.xcframework.zip",
+            checksum: "5590fdb72652f5965b708652651e12cca0e901494a42496fe2b9b57e0ec820ae"
+        ),
+        .binaryTarget(
+            name: "DocIDVAI-with-XS2A",
+            url: "https://github.com/idnow/docidv-ai-ios/releases/download/1.6.0/DocIDVAI-with-XS2A.xcframework.zip",
+            checksum: "66adc42128997e4ff8a670dba670d7d96f657b435d5a928bbc3263716bb680e3"
+        ),
+        // Define the third parties dependencies imported locally.
+        .binaryTarget(
+            name: "FaceTecSDK",
+            path: "Frameworks/FaceTecSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "ReadID_UI",
+            path: "Frameworks/ReadID_UI.xcframework"
+        ),
+        // Define a wrapper which will encapsulate every dependencies in it.
+        .target(
+            name: "DocIDVAI-Target-Wrapper",
+            dependencies: [
+                // Local DocIDVAI sdk binaries.
+                "DocIDVAI",
+                // External frameworks saved locally.
+                "FaceTecSDK",
+                "ReadID_UI",
+                // External public frameworks.
+                .product(name: "UnisseySdk", package: "sdk-ios"),
+                .product(name: "SunflowerUIKit", package: "sunflower-sdk-ios")
+            ],
+            path: "sources/DocIDVAI" // Path to an empty .swift file, needed by SPM.
+        ),
+        // Define a wrapper for 2nd target with XS2A library.
+        .target(
+            name: "DocIDVAI-with-XS2A-Target-Wrapper",
+            dependencies: [
+                // Local DocIDVAI sdk binaries (with XS2A lib).
+                "DocIDVAI-with-XS2A",
+                // External frameworks saved locally.
+                "FaceTecSDK",
+                "ReadID_UI",
+                // External public frameworks.
+                .product(name: "UnisseySdk", package: "sdk-ios"),
+                .product(name: "SunflowerUIKit", package: "sunflower-sdk-ios")
+            ],
+            path: "sources/DocIDVAI-with-XS2A" // Path to another empty .swift file, needed by SPM.
+        )
+    ]
+)
